c2969984e8eb6ab60db646a5b3c06ca504321320
Javier Aszerman <java@javans.tech>
2025-07-04 17:59:03 -0300

    Added support for data lifecycle policies

--- javans-custom.tf
+++ javans-custom.tf
@@ -112,4 +112,339 @@ resource "aws_security_group" "javans_all" {
       Name = "SG_JavaNS_ALL"
     }
   )
-} 
+}
+
+# # SSM private endpoints and settings
+# data "aws_region" "current" {}
+
+# resource "aws_security_group" "ssm_endpoints" {
+#   name        = "SG_SSM_Endpoints"
+#   description = "Allow VPC traffic to SSM endpoints"
+#   vpc_id      = local.vpc_id
+
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = [var.cidr]
+#     description = "Allow HTTPS from VPC"
+#   }
+
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+
+#   tags = merge(
+#     var.tags,
+#     {
+#       Name = "SG_SSM_Endpoints"
+#     }
+#   )
+# }
+
+# resource "aws_vpc_endpoint" "ssm" {
+#   vpc_id              = local.vpc_id
+#   service_name        = "com.amazonaws.${data.aws_region.current.name}.ssm"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = var.private_subnets
+#   security_group_ids  = [aws_security_group.ssm_endpoints.id]
+#   private_dns_enabled = true
+#   tags = merge(var.tags, { Name = "SSM Endpoint" })
+# }
+
+# resource "aws_vpc_endpoint" "ssmmessages" {
+#   vpc_id              = local.vpc_id
+#   service_name        = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = var.private_subnets
+#   security_group_ids  = [aws_security_group.ssm_endpoints.id]
+#   private_dns_enabled = true
+#   tags = merge(var.tags, { Name = "SSM Messages Endpoint" })
+# }
+
+# resource "aws_vpc_endpoint" "ec2messages" {
+#   vpc_id              = local.vpc_id
+#   service_name        = "com.amazonaws.${data.aws_region.current.name}.ec2messages"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = var.private_subnets
+#   security_group_ids  = [aws_security_group.ssm_endpoints.id]
+#   private_dns_enabled = true
+#   tags = merge(var.tags, { Name = "EC2 Messages Endpoint" })
+# }
+
+# resource "aws_vpc_endpoint" "logs" {
+#   vpc_id              = local.vpc_id
+#   service_name        = "com.amazonaws.${data.aws_region.current.name}.logs"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = var.private_subnets
+#   security_group_ids  = [aws_security_group.ssm_endpoints.id]
+#   private_dns_enabled = true
+#   tags = merge(var.tags, { Name = "CloudWatch Logs Endpoint" })
+# }
+
+# resource "aws_vpc_endpoint" "kms" {
+#   vpc_id              = local.vpc_id
+#   service_name        = "com.amazonaws.${data.aws_region.current.name}.kms"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = var.private_subnets
+#   security_group_ids  = [aws_security_group.ssm_endpoints.id]
+#   private_dns_enabled = true
+#   tags = merge(var.tags, { Name = "KMS Endpoint" })
+# }
+
+# resource "aws_vpc_endpoint" "s3" {
+#   vpc_id            = local.vpc_id
+#   service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+#   vpc_endpoint_type = "Gateway"
+#   route_table_ids   = local.private_route_table_ids
+#   tags = merge(var.tags, { Name = "S3 Gateway Endpoint" })
+# } 
+
+
+# IAM Role for Data Lifecycle Manager
+resource "aws_iam_role" "dlm_lifecycle_role" {
+  name = "dlm-lifecycle-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "dlm.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+# IAM Policy for Data Lifecycle Manager
+resource "aws_iam_role_policy" "dlm_lifecycle_policy" {
+  name = "dlm-lifecycle-policy"
+  role = aws_iam_role.dlm_lifecycle_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateSnapshot",
+          "ec2:CreateSnapshots",
+          "ec2:DeleteSnapshot",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeSnapshots"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateTags"
+        ]
+        Resource = "arn:aws:ec2:*::snapshot/*"
+      }
+    ]
+  })
+}
+
+# Data Lifecycle Manager - SnapHourly
+resource "aws_dlm_lifecycle_policy" "snap_hourly" {
+  description        = "SnapHourly - 4 hour frequency, 12 snapshots retention"
+  execution_role_arn = aws_iam_role.dlm_lifecycle_role.arn
+  state             = "ENABLED"
+
+  policy_details {
+    resource_types = ["INSTANCE"]
+
+    schedule {
+      name = "SnapHourly"
+
+      create_rule {
+        interval      = 4
+        interval_unit = "HOURS"
+        times         = ["00:00"]
+      }
+
+      retain_rule {
+        count = 12
+      }
+
+      tags_to_add = {
+        "SnapHourly" = "True"
+        "CreatedBy"  = "DLM"
+      }
+
+      copy_tags = true
+    }
+
+    target_tags = {
+      "SnapHourly" = "True"
+    }
+  }
+
+  tags = merge(var.tags, { Name = "SnapHourly-Lifecycle-Policy" })
+}
+
+# Data Lifecycle Manager - SnapDaily
+resource "aws_dlm_lifecycle_policy" "snap_daily" {
+  description        = "SnapDaily - 24 hour frequency, 14 snapshots retention"
+  execution_role_arn = aws_iam_role.dlm_lifecycle_role.arn
+  state             = "ENABLED"
+
+  policy_details {
+    resource_types = ["INSTANCE"]
+
+    schedule {
+      name = "SnapDaily"
+
+      create_rule {
+        interval      = 24
+        interval_unit = "HOURS"
+        times         = ["02:00"]
+      }
+
+      retain_rule {
+        count = 14
+      }
+
+      tags_to_add = {
+        "SnapDaily" = "True"
+        "CreatedBy" = "DLM"
+      }
+
+      copy_tags = true
+    }
+
+    target_tags = {
+      "SnapDaily" = "True"
+    }
+  }
+
+  tags = merge(var.tags, { Name = "SnapDaily-Lifecycle-Policy" })
+}
+
+# Data Lifecycle Manager - SnapWeekly
+resource "aws_dlm_lifecycle_policy" "snap_weekly" {
+  description        = "SnapWeekly - Weekly frequency, 8 snapshots retention"
+  execution_role_arn = aws_iam_role.dlm_lifecycle_role.arn
+  state             = "ENABLED"
+
+  policy_details {
+    resource_types = ["INSTANCE"]
+
+    schedule {
+      name = "SnapWeekly"
+
+      create_rule {
+        interval      = 1
+        interval_unit = "WEEKS"
+        times         = ["03:00"]
+        location      = "UTC"
+      }
+
+      retain_rule {
+        count = 8
+      }
+
+      tags_to_add = {
+        "SnapWeekly" = "True"
+        "CreatedBy"  = "DLM"
+      }
+
+      copy_tags = true
+    }
+
+    target_tags = {
+      "SnapWeekly" = "True"
+    }
+  }
+
+  tags = merge(var.tags, { Name = "SnapWeekly-Lifecycle-Policy" })
+}
+
+# Data Lifecycle Manager - SnapMonthly
+resource "aws_dlm_lifecycle_policy" "snap_monthly" {
+  description        = "SnapMonthly - Monthly frequency, 6 snapshots retention"
+  execution_role_arn = aws_iam_role.dlm_lifecycle_role.arn
+  state             = "ENABLED"
+
+  policy_details {
+    resource_types = ["INSTANCE"]
+
+    schedule {
+      name = "SnapMonthly"
+
+      create_rule {
+        interval      = 1
+        interval_unit = "MONTHS"
+        times         = ["04:00"]
+        location      = "UTC"
+      }
+
+      retain_rule {
+        count = 6
+      }
+
+      tags_to_add = {
+        "SnapMonthly" = "True"
+        "CreatedBy"   = "DLM"
+      }
+
+      copy_tags = true
+    }
+
+    target_tags = {
+      "SnapMonthly" = "True"
+    }
+  }
+
+  tags = merge(var.tags, { Name = "SnapMonthly-Lifecycle-Policy" })
+}
+
+# Data Lifecycle Manager - SnapYearly
+resource "aws_dlm_lifecycle_policy" "snap_yearly" {
+  description        = "SnapYearly - Yearly frequency, 3 years retention"
+  execution_role_arn = aws_iam_role.dlm_lifecycle_role.arn
+  state             = "ENABLED"
+
+  policy_details {
+    resource_types = ["INSTANCE"]
+
+    schedule {
+      name = "SnapYearly"
+
+      create_rule {
+        interval      = 1
+        interval_unit = "YEARS"
+        times         = ["05:00"]
+        location      = "UTC"
+      }
+
+      retain_rule {
+        count = 3
+      }
+
+      tags_to_add = {
+        "SnapYearly" = "True"
+        "CreatedBy"  = "DLM"
+      }
+
+      copy_tags = true
+    }
+
+    target_tags = {
+      "SnapYearly" = "True"
+    }
+  }
+
+  tags = merge(var.tags, { Name = "SnapYearly-Lifecycle-Policy" })
+}
+
